c1bf3bf5f8a850ede812a72355ce184529beb53e
yyczz <a729746954@gmail.com>
2024-05-01 00:16:55 +0800

    init

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-// import "solmate/tokens/ERC721.sol";
+import "solmate/tokens/ERC721.sol";
 
 contract Counter {
     uint256 public number;
